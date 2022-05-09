e5e46a9f77d607ab3be5aeb8036bb63f9341a1c0
MarkMinia <100399062+MarkMinia@users.noreply.github.com>
2022-05-08 22:21:49 -0700

    Delete 1.1.sql

--- 1.1.sql
+++
@@ -1,5 +0,0 @@
-select b.bandname as 'Rockin Rock Bands', a.albumname as 'Awesome Album Names', a.releasedate as 'Released to the World'
-from band_db.band as b
-join band_db.album as a
-on b.idband = a.idband
-order by a.releasedate desc
